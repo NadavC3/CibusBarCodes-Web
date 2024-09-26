47fefa3c2b9bc0bccf6839a3fbf3fe5f4c958942
NadavC3 <nadav3395@gmail.com>
2024-09-26 16:29:38 +0300

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    //baseUrl: 'https://cibus.onrender.com', //  API base URL
-    baseUrl: 'http://192.168.31.184:8000',
+    baseUrl: 'https://cibusbarcodes-web.onrender.com', //  API base URL
+    //baseUrl: 'http://192.168.31.184:8000',
   };
   
   export default config;
