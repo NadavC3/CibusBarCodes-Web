ced24d4db7b621cdad1a25c5a7ea18e9d7eb12a5
NadavC3 <nadav3395@gmail.com>
2025-02-27 13:05:22 +0200

    Fixed filtering coupons
    Fixed filtering coupons

--- frontend/src/components/Coupon.jsx
+++ frontend/src/components/Coupon.jsx
@@ -39,7 +39,7 @@ const Coupons = ({ userId }) => {
     };
 
     fetchCoupons();
-  }, [userId, coupons]);
+  }, [userId]);
 
   const handleAddCoupon = async () => {
     try {
@@ -53,9 +53,9 @@ const Coupons = ({ userId }) => {
       });
       setSmsMessage('');
       onAddClose();
-      // setTimeout(() => {
-      //   window.location.reload(); 
-      // }, 3000);
+      setTimeout(() => {
+        window.location.reload(); 
+      }, 500);
     } catch (error) {
       toast({
         title: "Error adding coupon.",
